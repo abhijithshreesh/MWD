3284d74d4593918a73dcb7d23757c30caf3cff91
sahu911@gmail.com <sahu911@gmail.com>
2017-10-07 16:48:53 -0700

    Adding code for task 1 and 2

---
+++ phase_2_task_2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import logging
+from config_parser import ParseConfig
+from task_2 import GenreTag
+import argparse
+import scipy
+import numpy
+from sklearn.decomposition import TruncatedSVD
+logging.basicConfig(level=logging.INFO)
+
+log = logging.getLogger(__name__)
+conf = ParseConfig()
+
+class SvdGenreTag(GenreTag):
+    def __init__(self):
+        super().__init__()
+        self.data_set_loc = conf.config_section_mapper("filePath").get("data_set_loc")
+
+    def genre_tag(self, genre):
+        """
+        Triggers the compute function and outputs the result tag vector
+        :param genre:
+        :param model:
+        :return: returns a dictionary of Genres to dictionary of tags and weights.
+        """
+
+        genre_tag_frame = self.get_genre_data()
+        given_genre_frame = self.combine_computed_weights(genre_tag_frame, "TFIDF", genre)
+        temp_df = given_genre_frame[["moviename", "tag", "total"]].drop_duplicates()
+        genre_tag_tfidf_df = temp_df.pivot(index='moviename', columns='tag', values='total')
+        genre_tag_tfidf_df = genre_tag_tfidf_df.fillna(0)
+        U, s, Vh = numpy.linalg.svd(genre_tag_tfidf_df.values,full_matrices=True)
+        a = 1
+
+        x = genre_tag_tfidf_df.values
+        svd = TruncatedSVD(n_components = 4)
+        svd.fit(x)
+        print(svd.explained_variance_ratio_)
+        print(svd.components_)
+
+if __name__ == "__main__":
+    obj = SvdGenreTag()
+    obj.genre_tag(genre="Action")
