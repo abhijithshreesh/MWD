e71d1486953a8949d3813961f5118429986db44b
Aditya <aditya-may9@users.noreply.github.com>
2017-10-07 18:07:39 -0700

    modified files

--- scripts/config_parser.py
+++ scripts/config_parser.py
@@ -7,27 +7,20 @@ log = logging.getLogger(__name__)
 
 
 class ParseConfig(object):
-    """
-    Class to extract information from the config.Ini file
-    """
-
     def __init__(self):
-        self.Config = configparser.ConfigParser()
-        self.Config.read("../config.ini")
+        self.config = configparser.ConfigParser()
+        self.config.read("../config.ini")
 
-    def config_section_mapper(self, section):
-            dict1 = {}
-            options = self.Config.options(section)
-            for option in options:
-                try:
-                    dict1[option] = self.Config.get(section, option)
-                    if dict1[option] == -1:
-                        log.debug("skip: %s" % option)
-                except:
-                    log.error("exception on %s!" % option)
-                    dict1[option] = None
-            return dict1
+    def config_section_mapper(self, section):  # Code to parse a section of the config.ini file
+        dict1 = {}
+        options = self.config.options(section)
+        for option in options:
+            dict1[option] = self.config.get(section, option)
+
+        return dict1
+
 
 if __name__ == "__main__":
-    conf = ParseConfig()
-    print conf.config_section_mapper("filePath")
+    parsed_config = ParseConfig()
+    log.info("Parsed config file")
+    log.info(parsed_config.config_section_mapper("filePath"))
--- scripts/data_extractor.py
+++ scripts/data_extractor.py
@@ -3,47 +3,47 @@ import os
 
 import pandas as pd
 
-from config_parser import ParseConfig
+import config_parser
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-class ExtractData(object):
-    """
-    Wrapper class to extract the data from the csv files
-    """
-    def __init__(self, data_set_loc):
-        self.data_set_loc = data_set_loc
+class DataExtractor(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-    def data_extractor(self, file_name):
-        file_loc = os.path.join(self.data_set_loc, file_name)
-        log.info("Loading data from %s to dataframe" % file_loc)
-        data_frame = pd.DataFrame.from_csv(file_loc)
-        log.info("%s dataframe loaded with the index = %s, columns = %s and rows =%s" % (file_name,data_frame.index.name, str(data_frame.columns.values), len(data_frame.index)))
+    def data_extractor(self, file_name):  # return the data frame with respect to the csv file in 'resources' directory
+        file_loc = os.path.join(self.file_path, file_name)
+        data_frame = pd.read_csv(file_loc)
         return data_frame
 
-
     def get_movie_actor_data(self):
-        return self.data_extractor("movie-actor.csv").reset_index()
+        return self.data_extractor("movie-actor.csv")
 
-    def get_ml_tags_data(self):
-        return self.data_extractor("mltags.csv").reset_index()
+    def get_mltags_data(self):
+        return self.data_extractor("mltags.csv")
 
     def get_genome_tags_data(self):
-        return self.data_extractor("genome-tags.csv").reset_index()
+        return self.data_extractor("genome-tags.csv")
 
-    def get_imdb_actor_info(self):
-        return self.data_extractor("imdb-actor-info.csv").reset_index()
+    def get_mlmovies_data(self):
+        return self.data_extractor("mlmovies.csv")
 
-    def get_movie_genre_data(self):
-        return self.data_extractor("mlmovies.csv").reset_index()
+    def get_imdb_actor_info_data(self):
+        return self.data_extractor("imdb-actor-info.csv")
 
+    def get_mlratings_data(self):
+        return self.data_extractor("mlratings.csv")
 
+    def get_mlusers_data(self):
+        return self.data_extractor("mlusers.csv")
 
 
 if __name__ == "__main__":
-    conf = ParseConfig()
-    data_set_loc = conf.config_section_mapper("filePath").get("data_set_loc")
-    e_d = ExtractData(data_set_loc)
-    print e_d.data_extractor("mlmovies.csv")
+    conf = config_parser.ParseConfig()
+    data_set_location = conf.config_section_mapper("filePath").get("data_set_location")
+    extract_data = DataExtractor(data_set_location)
+    data_frame = extract_data.data_extractor("mlmovies.csv")
+    log.info("File columns for mlmovies.csv")
+    log.info("Columns = %s" % (data_frame.columns.values))
