1cdce069d9b0e4011c776f3ed0498229abedb365
Jagdeesh Basavaraju <jagdeeshb14@gmail.com>
2017-10-10 03:55:12 -0700

    Task 1c TF-IDF tag vectors

---
+++ scripts/phase_2_task_1c_tfidf.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import logging
+from config_parser import ParseConfig
+from data_extractor import DataExtractor
+from collections import Counter
+import math
+import argparse
+import operator
+from actor_actor_similarity_matrix import ActorActorMatrix
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+conf = ParseConfig()
+
+class SimilarActors(ActorActorMatrix):
+
+    def __init__(self):
+        """
+        Initialiazing the data extractor object to get data from the csv files
+        """
+        self.data_set_loc = conf.config_section_mapper("filePath").get("data_set_loc")
+        self.data_extractor = DataExtractor(self.data_set_loc)
+
+    def get_actor_actor_vector(self, actorid):
+        (matrix, actorids) = self.getActorActorMatrix()
+        index_actor = None
+        for i,j in enumerate(actorids):
+            if j == actorid:
+                index_actor = i
+                break
+
+        if index_actor==None:
+            print ("Actor Id not found.")
+            return None
+
+        actor_row = matrix[index_actor].tolist()
+        actor_actor_dict = dict(zip(actorids, actor_row))
+        del actor_actor_dict[actorid]
+        actor_actor_dict = sorted(actor_actor_dict.items(), key=operator.itemgetter(1), reverse=True)
+        return actor_actor_dict
+
+
+if __name__ == "__main__":
+    obj = SimilarActors()
+    actor_actor_dict = obj.get_actor_actor_vector(actorid=542238)
+    print (actor_actor_dict)
