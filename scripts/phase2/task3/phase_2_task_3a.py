20adda68d294ba9bd500c4dd99cc53181099d966
Aditya <aditya-may9@users.noreply.github.com>
2017-10-17 13:00:19 -0700

    Abhijith-Task 3

--- scripts/phase2/task3/phase_2_task_3a.py
+++ scripts/phase2/task3/phase_2_task_3a.py
@@ -1,14 +1,13 @@
-import pandas as pd
 import logging
-from scripts.phase2.common.config_parser import ParseConfig
-from scripts.phase2.common.data_extractor import DataExtractor
-from collections import Counter
-import math
-import argparse
-import operator
-from scripts.phase2.common.actor_actor_similarity_matrix import ActorActorMatrix
+
 import networkx as nx
 import numpy as np
+import pandas as pd
+
+from scripts.phase2.common.actor_actor_similarity_matrix import ActorActorMatrix
+from scripts.phase2.common.config_parser import ParseConfig
+from scripts.phase2.common.data_extractor import DataExtractor
+
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
@@ -24,7 +23,9 @@ class PageRankActor(ActorActorMatrix):
         actor_matrix, actorids = self.fetchActorActorSimilarityMatrix()
         actor_df = pd.DataFrame(actor_matrix)
         for column in actor_df:
-            actor_df[column] = pd.Series([int(column) if each>0 else 0 for each in actor_df[column]], index= actor_df.index)
+            actor_df[column] = pd.Series([1 if each > 0 else 0 for each in actor_df[column]], index=actor_df.index)
+        temp_df = actor_df[(actor_df.T == 0).any()]
+        actor_df.loc[(actor_df.T == 0).any()] = 1 / (len(temp_df.columns))
         graph = nx.from_numpy_matrix(np.array(actor_df.values))
         pagerank_dict = nx.pagerank(graph)
         a =1
