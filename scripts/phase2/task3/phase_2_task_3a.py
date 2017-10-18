e512af1a0251cac99d367f3956d18242cf7dcac8
abhijith-shreesh <abhijithshreesh@gmail.com>
2017-10-17 21:47:21 -0700

    phase_2 task_3

--- scripts/phase2/task3/phase_2_task_3a.py
+++ scripts/phase2/task3/phase_2_task_3a.py
@@ -27,15 +27,18 @@ class PageRankActor(ActorActorMatrix):
             actor_df[column] = pd.Series(
                 [1 if (each > 0 and ind != int(column)) else 0 for ind, each in zip(actor_df.index, actor_df[column])],
                 index=actor_df.index)
-        seed_frame = pd.DataFrame(0 ,index=np.arange(len(actor_df.columns)), columns=actor_df.columns)
-        seed_matrix = seed_frame.values
+        seed_frame = pd.DataFrame(0.0, index=np.arange(len(actor_df.columns)), columns=actor_df.columns)
+        seed_value = float(1/len(seed_actors))
         for each in seed_actors:
-            seed_matrix[list(actorids).index(each), list(actorids).index(each)] = 1/len(seed_actors)
+            seed_frame[list(actorids).index(each)] = seed_value
+        seed_matrix = seed_frame.values
         actor_df.loc[(actor_df.T == 0).all()] = 1 / (len(actor_df.columns))
         res = 0.85*actor_df.values + 0.15*seed_matrix
         e_values, e_vectors = scipy.sparse.linalg.eigsh(res, k=1, sigma=1)
-        page_rank_dict = {i:round(float(j[0]), 10) for i,j in zip(actorids, e_vectors)}
+        page_rank_dict = {i: round(float(j[0]), 10) for i, j in zip(actorids, e_vectors)}
         sorted_rank = sorted(page_rank_dict.items(), key=operator.itemgetter(1), reverse=True)
+        print (sorted_rank[0:10])
+
         # personalization = {each: 0 for each in actorids.index}
         # personalization[list(actorids).index(581911)] = 1
         # graph = nx.from_numpy_matrix(np.array(actor_df.values))
