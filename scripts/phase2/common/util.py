79c2c5f99b1b3dc3a796d7a23d0e1882f3ebce76
Aditya <aditya-may9@users.noreply.github.com>
2017-10-19 21:04:19 -0700

    Added common functions to utils

--- scripts/phase2/common/util.py
+++ scripts/phase2/common/util.py
@@ -1,14 +1,5 @@
-import logging
-import math
-from collections import Counter
-
-import pandas as pd
-
 from scripts.phase2.common.config_parser import ParseConfig
 from scripts.phase2.common.data_extractor import DataExtractor
-
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
 
 conf = ParseConfig()
 
@@ -25,12 +16,34 @@ class Util(object):
         """
         self.data_set_loc = conf.config_section_mapper("filePath").get("data_set_loc")
         self.data_extractor = DataExtractor(self.data_set_loc)
+        self.mlratings = self.data_extractor.get_mlratings_data()
+        self.mlmovies = self.data_extractor.get_mlmovies_data()
 
     def get_sorted_actor_ids(self):
         actor_info = self.data_extractor.get_imdb_actor_info_data()
         actorids = actor_info.id
         actorids = actorids.sort_values()
         return actorids
+
+    def get_movie_id(self, movie):
+        all_movie_data = self.mlmovies
+        movie_data = all_movie_data[all_movie_data['moviename'] == movie]
+        movie_id = movie_data['movieid'].unique()
+
+        return movie_id[0]
+
+    def get_average_ratings_for_movie(self, movie_id):
+        all_ratings = self.mlratings
+        movie_ratings = all_ratings[all_ratings['movieid'] == movie_id]
+
+        ratings_sum = 0
+        ratings_count = 0
+        for index, row in movie_ratings.iterrows():
+            ratings_count += 1
+            ratings_sum += row['rating']
+
+        return ratings_sum / float(ratings_count)
+
 
 if __name__ == "__main__":
     obj = Util()
