ea840c40b021a03617dfd0d665e2b211c07a0e75
kthoma46 <kthoma46@asu.edu>
2017-10-20 16:53:04 -0700

    Updated Task 2c

--- scripts/phase2/task2/phase_2_task_2c.py
+++ scripts/phase2/task2/phase_2_task_2c.py
@@ -7,16 +7,16 @@ from scripts.phase2.common.util import Util
 
 class ActorMovieYearTensor(object):
 
-    ordered_years = []
-    ordered_movie_names = []
-    ordered_actor_names = []
-
     def __init__(self):
         self.conf = ParseConfig()
         self.data_set_loc = self.conf.config_section_mapper("filePath").get("data_set_loc")
         self.data_extractor = DataExtractor(self.data_set_loc)
-        self.tensor = self.fetchActorMovieYearTensor()
+        self.ordered_years = []
+        self.ordered_movie_names = []
+        self.ordered_actor_names = []
+        self.print_list = ["\n\nFor Years:\n", "\n\nFor Movies:\n", "\n\nFor Actors:\n"]
         self.util = Util()
+        self.tensor = self.fetchActorMovieYearTensor()
         self.factors = self.util.CPDecomposition(self.tensor, 5)
 
     def fetchActorMovieYearTensor(self):
@@ -34,7 +34,6 @@ class ActorMovieYearTensor(object):
             year_count += 1
             self.ordered_years.append(element)
 
-        util = Util()
         movieid_list = movie_actor_df["movieid"]
         movieid_count = 0
         movieid_dict = {}
@@ -43,7 +42,7 @@ class ActorMovieYearTensor(object):
                 continue
             movieid_dict[element] = movieid_count
             movieid_count += 1
-            name = util.get_movie_name_for_id(element)
+            name = self.util.get_movie_name_for_id(element)
             self.ordered_movie_names.append(name)
 
         actorid_list = movie_actor_df["actorid"]
@@ -54,7 +53,7 @@ class ActorMovieYearTensor(object):
                 continue
             actorid_dict[element] = actorid_count
             actorid_count += 1
-            name = util.get_actor_name_for_id(element)
+            name = self.util.get_actor_name_for_id(element)
             self.ordered_actor_names.append(name)
 
         tensor = np.zeros((year_count + 1, movieid_count + 1, actorid_count + 1))
@@ -73,23 +72,38 @@ class ActorMovieYearTensor(object):
     def print_latent_semantics(self, r):
         i = 0
         for factor in self.factors:
+            print(self.print_list[i])
             latent_semantics = self.util.get_latent_semantics(r, factor.transpose())
             self.util.print_latent_semantics(latent_semantics, self.get_factor_names(i))
             i+=1
 
     def get_factor_names(self, i):
         if i == 0:
-            print("\n\nFor Years:\n")
             return self.ordered_years
         elif i == 1:
-            print("\n\nFor Movies:\n")
             return self.ordered_movie_names
         elif i == 2:
-            print("\n\nFor Actors:\n")
             return self.ordered_actor_names
 
+    def get_partitions(self, no_of_partitions):
+        i = 0
+        groupings_list = []
+        for factor in self.factors:
+            groupings = self.util.partition_factor_matrix(factor.transpose(), no_of_partitions, self.get_factor_names(i))
+            groupings_list.append(groupings)
+            i += 1
+
+        return groupings_list
+
+    def print_partitioned_entites(self, no_of_partitions):
+        groupings_list = self.get_partitions(no_of_partitions)
+        i = 0
+        for groupings in groupings_list:
+            print(self.print_list[i])
+            self.util.print_partitioned_entities(groupings)
+            i += 1
 
 if __name__ == "__main__":
     obj = ActorMovieYearTensor()
     obj.print_latent_semantics(5)
-    # obj.print_partitioned_entites(5)
+    obj.print_partitioned_entites(5)
