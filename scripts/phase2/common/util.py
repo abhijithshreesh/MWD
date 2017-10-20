be209a27cf61403b56ee0c1ce10a397e83cf75ef
Aditya <aditya-may9@users.noreply.github.com>
2017-10-19 23:41:30 -0700

    Task 2a

--- scripts/phase2/common/util.py
+++ scripts/phase2/common/util.py
@@ -1,3 +1,5 @@
+import math
+
 from scripts.phase2.common.config_parser import ParseConfig
 from scripts.phase2.common.data_extractor import DataExtractor
 
@@ -18,6 +20,7 @@ class Util(object):
         self.data_extractor = DataExtractor(self.data_set_loc)
         self.mlratings = self.data_extractor.get_mlratings_data()
         self.mlmovies = self.data_extractor.get_mlmovies_data()
+        self.imdb_actor_info = self.data_extractor.get_imdb_actor_info_data()
 
     def get_sorted_actor_ids(self):
         actor_info = self.data_extractor.get_imdb_actor_info_data()
@@ -43,6 +46,38 @@ class Util(object):
             ratings_sum += row['rating']
 
         return ratings_sum / float(ratings_count)
+
+    def get_actor_name_for_id(self, actor_id):
+        actor_data = self.imdb_actor_info[self.imdb_actor_info['id'] == actor_id]
+        name = actor_data['name'].unique()
+
+        return name[0]
+
+    def partition_factor_matrix(self, matrix, no_of_partitions, entity_names):
+        entity_dict = {}
+        for i in range(0, len(matrix)):
+            length = 0
+            for latent_semantic in matrix[i]:
+                length += abs(latent_semantic)
+            entity_dict[entity_names[i]] = length
+
+        max_length = max(entity_dict.values())
+        min_length = min(entity_dict.values())
+        length_of_group = (float(max_length) - float(min_length)) / float(no_of_partitions)
+
+        groups = {}
+        for key in entity_dict.keys():
+            entity_length = entity_dict[key]
+            group_no = math.ceil(float(entity_length - min_length) / float(length_of_group))
+            if group_no == 0:
+                group_no = 1
+            if "Group " + str(group_no) in groups.keys():
+                groups["Group " + str(group_no)].append(key)
+            else:
+                groups["Group " + str(group_no)] = []
+                groups["Group " + str(group_no)].append(key)
+
+        return groups
 
 
 if __name__ == "__main__":
--- scripts/phase2/task2/phase_2_task_2a.py
+++ scripts/phase2/task2/phase_2_task_2a.py
@@ -1,18 +1,58 @@
 import numpy
 
 from scripts.phase2.common.actor_actor_similarity_matrix import ActorActorMatrix
+from scripts.phase2.common.util import Util
 
 
 class ActorActorSVD(object):
-    def actor_actor_svd(self):
-        actor_actor_matrix_object = ActorActorMatrix()
-        actor_actor_similarity_matrix, actor_ids = actor_actor_matrix_object.fetchActorActorSimilarityMatrix()
-        U, s, V = numpy.linalg.svd(actor_actor_similarity_matrix, full_matrices=False)
-        for list in V:
-            print("\n\n\n")
-            print(list)
+    def __init__(self):
+        self.actor_actor_matrix_object = ActorActorMatrix()
+        self.actor_actor_similarity_matrix, self.actor_ids = self.actor_actor_matrix_object.fetchActorActorSimilarityMatrix()
+        self.u, self.s, self.v = numpy.linalg.svd(self.actor_actor_similarity_matrix, full_matrices=False)
+        self.util = Util()
+
+    def get_latent_semantics(self, r):
+        latent_semantics = []
+        for latent_semantic in self.v:
+            if len(latent_semantics) == r:
+                break
+            latent_semantics.append(latent_semantic)
+
+        return latent_semantics
+
+    def get_partitions(self, no_of_partitions):
+        actor_names_list = []
+        for actor in self.actor_ids:
+            actor_names_list.append(self.util.get_actor_name_for_id(actor))
+        groupings = self.util.partition_factor_matrix(self.u, no_of_partitions, actor_names_list)
+
+        return groupings
+
+    def print_partitioned_actors(self, no_of_partitions):
+        groupings = self.get_partitions(no_of_partitions)
+        for key in groupings.keys():
+            print(str(key) + " Actors")
+            for actor in groupings[key]:
+                print(actor, end="|")
+            print("\n\n")
+
+    def print_latent_semantics(self, r):
+        latent_semantics = self.get_latent_semantics(r)
+        actor_names_list = []
+        for actor in self.actor_ids:
+            actor_names_list.append(self.util.get_actor_name_for_id(actor))
+
+        for latent_semantic in latent_semantics:
+            print("Latent Semantic in terms of Actors:")
+            for i in range(0, len(actor_names_list)):
+                print(str(latent_semantic[i]) + "*(" + str(actor_names_list[i]) + ")", end="")
+                if i != len(actor_names_list) - 1:
+                    print(" + ", end="")
+            print("\n\n")
 
 
 if __name__ == "__main__":
     obj = ActorActorSVD()
-    obj.actor_actor_svd()
+    obj.print_latent_semantics(3)
+    print("\n\n\n")
+    obj.print_partitioned_actors(3)
