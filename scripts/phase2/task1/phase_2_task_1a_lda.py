6c65cdcd05437eee15c624b6471290ec8b7f86d9
kthoma46 <kthoma46@asu.edu>
2017-10-20 18:04:45 -0700

    Abhijith - tag count update

--- scripts/phase2/task1/phase_2_task_1a_lda.py
+++ scripts/phase2/task1/phase_2_task_1a_lda.py
@@ -5,6 +5,8 @@ from scripts.phase2.common.task_2 import GenreTag
 from sklearn.preprocessing import Imputer
 from sklearn.decomposition import LatentDirichletAllocation
 import argparse
+from collections import Counter
+
 #import gensim
 logging.basicConfig(level=logging.INFO)
 
@@ -16,20 +18,28 @@ class LdaGenreTag(GenreTag):
         super().__init__()
         self.data_set_loc = conf.config_section_mapper("filePath").get("data_set_loc")
 
+
+    def get_tag_count(self, tag_series):
+        counter = Counter()
+        for each in tag_series:
+            counter[each] += 1
+        return dict(counter)
+
     def get_lda_data(self, genre):
          data_frame = self.get_genre_data().reset_index()
          genre_data_frame = data_frame[data_frame["genre"]==genre]
-         tf_df = genre_data_frame.groupby(['movieid'])['tag'].apply(lambda x: ','.join(x)).reset_index()
-         movie_tag_dict = dict(zip(tf_df.movieid, tf_df.tag))
-         tf_weight_dict = {movie: self.assign_tf_weight(tags.split(',')) for movie, tags in
+         tag_count_df = genre_data_frame.groupby(['movieid'])['tag'].apply(lambda x: ','.join(x)).reset_index()
+         movie_tag_dict = dict(zip(tag_count_df.movieid, tag_count_df.tag))
+         count_dict = {movie: self.get_tag_count(tags.split(',')) for movie, tags in
                            list(movie_tag_dict.items())}
-         tag_df = self.get_model_weight(tf_weight_dict, {}, genre_data_frame, "TF")
-         tag_df["value"] = pd.Series([tf_weight_dict.get(movieid, 0).get(tag, 0) for index, tag, movieid
+         tag_df = self.get_model_weight(count_dict, {}, genre_data_frame, "TF")
+         tag_df["value"] = pd.Series([count_dict.get(movieid, 0).get(tag, 0) for index, tag, movieid
                              in zip(tag_df.index, tag_df.tag, tag_df.movieid)], index=tag_df.index)
          #tag_df["total"] = tag_df.groupby(['movieid', 'tag'])['value'].transform('sum')
          tag_df = tag_df.drop_duplicates("tag").sort_values("value", ascending=False)
          temp_df = tag_df[["moviename", "tag", "value"]].drop_duplicates()
-         genre_tag_freq = temp_df.pivot(index='moviename', columns='tag', values='value') #, fill_value=0)
+         genre_tag_freq = temp_df.pivot_table(index='moviename', columns='tag', values='value')
+        # genre_tag_freq = temp_df.pivot(index='moviename', columns='tag', values='value') #, fill_value=0)
          genre_tag_freq = genre_tag_freq.fillna(0)
          lda = LatentDirichletAllocation(n_components=4)
          lda.fit(genre_tag_freq.values)
